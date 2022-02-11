dd8ab124281f0e40cf76e8b43054b67f3b67aa0a
Shriya <93962518+shriyasankhyan@users.noreply.github.com>
2022-02-11 06:21:33 +0530

    Add files via upload

---
+++ Arrays/K008.java
@@ -0,0 +1,41 @@
+public class K008 {
+    public static int[] searchRange(int[] nums, int target) {
+        int start = search(nums,target,true);
+        int end = search(nums,target,false);
+        return new int [] {start,end};
+    }
+
+//    This function just return the index value of the target.
+    static int search(int[] nums, int target, boolean findStartIndex){
+//        Potential answer
+        int ans = -1;
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = start +(end-start)/2;
+            if(target < nums[mid]){
+                end = mid-1;
+            }
+            else if(target > nums[mid]){
+                start = mid+1;
+            }
+            else {
+                ans = mid;
+                if(findStartIndex ){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int [] nums = {5,7,7,8,8,10};
+        int target = 8;
+        for(int element : searchRange(nums,target)){
+            System.out.print(element +" ");
+        }
+    }
+}
