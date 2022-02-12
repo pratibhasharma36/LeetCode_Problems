b996fe1a274a25cb22eb1ba02c450f8bc8679c5b
Shriya <93962518+shriyasankhyan@users.noreply.github.com>
2022-02-12 15:22:36 +0530

    Delete K014.java

--- SearchAlgorithms/K014.java
+++
@@ -1,9 +0,0 @@
-package SearchAlgorithms;
-
-public class K014 {
-//    We have to find the sub-array which has the largest sum.
-//    Largest sub-array = Total array
-//    Smallest sub-array = 1 element array
-//  Answer will be greater than the maximum element in the array and as all are positive integers (as per question) ,
-//  then it will be the largest when entire array sum is taken.
-}
