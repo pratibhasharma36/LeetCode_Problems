5720c9d87605a213d77bf35ecf8b68dd5836b0b6
Shriya <93962518+shriyasankhyan@users.noreply.github.com>
2022-02-12 09:33:26 +0530

    Add files via upload

---
+++ Arrays/K010.java
@@ -0,0 +1,15 @@
+public class K010 {
+    public static void main(String[] args) {
+        int [] arr ={-3,-2,0,2,5,6,4,3,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int i = 1;
+        int max = -1;
+        while(i<arr.length && arr[i-1] < arr[i]){
+            max = i;
+            i++;
+        }
+        return max;
+    }
+}
